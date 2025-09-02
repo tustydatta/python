1d5b8abb8036191543eeaf7ec201ffd8ea54dc64
tustydatta <tustydatta9@gmail.com>
2025-09-02 22:47:04 +0600

    lacture2_3

---
+++ 01.py
@@ -0,0 +1,96 @@
+# print("Hello World")
+
+# name = "Tusty"
+# age = 23
+# price = 35.53
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# a = 5 
+# b = 2
+# print(a ** b) #power
+
+# name = input("Enter your name: ")
+# print("Welcome ", name)
+
+# name = input("enter name: ")
+# age = int(input("enter age: "))
+# marks = float(input("enter marks: "))
+
+# print("welcome ", name)
+# print("age = ", age)
+# print("marks = ", marks)
+
+# a = int(input("enter 1st num: "))
+# b = int(input("enter 2nd num: "))
+# sum = a + b
+# print(sum)
+
+# a = int(input("enter the size of a square: "))
+# print("area: ", a*a)
+# print("area: ", a ** 2)
+
+# a = float(input())
+# b = float(input())
+# avg = (a+b)/2
+# print("average", avg)
+
+# a = int(input())
+# b = int(input())
+# print(a >= b)
+
+# str = "apna college"
+# print(str[1:4])
+# print(str[5:len(str)]) #len(str) - string length
+# print(str[:4]) #[0:4]
+# print(str[5:]) #[5:len(str)]
+
+# str = "Apple"
+# print(str[-3:-1]) # cheracter skip
+
+# str = "i am from studying python from ApnaCollege"
+# print(str.endswith("ege"))
+# print(str.capitalize())
+# print(str.replace("a", "A"))
+# print(str.find("from"))
+# print(str.count("from"))
+
+# first_name = str(input())
+# print(len(first_name))
+
+# str = "HI, $iam the $ symbol $ $99,666"
+# print(str.count("$"))
+
+# list = [2, 1, 3]
+# print(list.append(4))
+# print(list.sort())
+# print(list)
+# list.sort(reverse=True)
+# print(list)
+# list.insert(1, 5)
+# print(list)
+# list1 = ['a', 'd', 'e', 'f', 'c', 'b']
+# list1.reverse()
+# print(list1)
+
+# Q. ask the user to enter names of their 3 favorite movies & store them in a list.
+# movies = []
+# m1 = input("1st movie: ")
+# m2 = input("2nd movie: ")
+# m3 = input("3rd movie: ")
+# movies.append(m1)
+# movies.append(m2)
+# movies.append(m3)
+# print(movies)
+
+# Q. check if a list contains a palindrome of elements. (Hint: use copy() method)
+# list1 = [1, 2, 1]
+# list2 = [1, 2, 3]
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+# if(copy_list1 == list1):
+#     print("palindrome")
+# else: 
+#     print("Not palindrome")
