006e780c4f30b13bd57609f61c9f9e86cf2d9d0e
tustydatta <tustydatta9@gmail.com>
2025-09-18 20:40:20 +0600

    Add cicd

---
+++ first.py
@@ -0,0 +1,14 @@
+
+# print("Hello, Python!")
+# if 5 > 2:
+#     print("Five is greater than two!")
+
+# x = 5
+# y = "Hello, World!"
+# print(x)
+# print(y)
+# print("tusty")
+# print("Opu")
+# print("shorna")
+import sys 
+print(sys.version)
